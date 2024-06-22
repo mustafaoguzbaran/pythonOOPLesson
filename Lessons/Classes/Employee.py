90d2636fd1b81929919041d428438477e8352280
mustafaoguzbaran <mustafa.baran@easycep.com>
2024-06-22 12:57:39 +0300

    mustafaoguzbaran: classes lesson was created.

---
+++ Lessons/Classes/Employee.py
@@ -0,0 +1,11 @@
+#Class: Ortak özelliklere sahip nesnelerin bir arada toplanmış halidir. Class'lar bir objedir bu objeler hem bir
+# şeyler yaparlar hem de bir şeyleri bilirler.
+class Employee:
+    #İçinde bulunacak olanlar:
+    #attribute: Özellik anlamına gelir. Örneğin Ahmet'in maaşı, departmanı, yaşı, ismi, soyismi vb.
+    #behaviour: Davranı. anlamına gelir. Ahmet'in departmanı eğer IT departmanı ise Ahmet'in kod yazması onun bir
+    # davranışı anlamına gelir.
+    pass
+
+# Class yaratma aşağıdaki gibi yapılır.
+employee1 = Employee()
